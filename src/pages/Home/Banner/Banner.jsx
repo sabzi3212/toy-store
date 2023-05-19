827d9a5393fdbb9b2a4dfd9f4cc82705141fd4d2
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-05-19 17:53:24 +0600

    banner added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,32 +1,33 @@
+import img1 from '../../../../assets/carousel/21186291_hg8t_5r7d_210702.jpg'
+import img2 from '../../../../assets/carousel/28458919_iu49_yhip_220331.jpg'
+import img3 from '../../../../assets/carousel/vintage-miniature-car-fake-landscape.jpg'
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full h-[600px] mt-6">
             <div id="slide1" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+                <img src={img1} className='w-full'/>
+                <div className="absolute flex justify-between transform -translate--1/2 left-10 right-3 top-1/2">
+                    <div>
+                        <h2 className='text-7xl text-center'>Find best toys for your kid</h2>
+                    </div>
+                </div>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide4" className="btn btn-circle">❮</a>
                     <a href="#slide2" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+                <img src={img2} className="w-full" />
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide1" className="btn btn-circle">❮</a>
                     <a href="#slide3" className="btn btn-circle">❯</a>
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+                <img src={img3} className='w-full'/>
                 <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <a href="#slide2" className="btn btn-circle">❮</a>
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                </div>
-            </div>
-            <div id="slide4" className="carousel-item relative w-full">
-                <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a>
                     <a href="#slide1" className="btn btn-circle">❯</a>
                 </div>
             </div>
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from "../Banner/Banner";
 
+
 const Home = () => {
     return (
         <div>
             <h3>This is home</h3>
             <Banner></Banner>
+            
         </div>
     );
 };
