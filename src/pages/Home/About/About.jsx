8d0b04551a20b54f45c3e50b30afdeb58d655e2b
sabzi3212 <sabbirahmed.lu1@gmail.com>
2023-05-19 22:38:59 +0600

    product section updated

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div className="hero min-h-screen bg-base-200 mt-4">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className='bg-purple-50'>
+            <h2 className='text-7xl font-bold text-center mt-4'>About Us</h2>
+        <div className="hero min-h-full mt-14">
+            
+            
+  <div className="hero-content flex-col lg:flex-row">
     <img src="https://i.ibb.co/Sry8rxq/yuri-shirota-p0h-Dzt-R46cw-unsplash.jpg" className="max-w-sm rounded-lg shadow-2xl" />
     <div data-aos="flip-left">
-      <h1 className="text-5xl font-bold">Box Office News!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-      <button className="btn btn-primary">Get Started</button>
+      <h1 className="text-5xl font-bold">Spark Your Child's Imagination with Our Toys!</h1>
+      <p className="py-6">At our toy store, we pride ourselves on offering an exceptional selection of toys that cater to a wide range of ages, interests, and developmental stages. From educational and interactive toys to classic favorites and the latest trends, we carefully curate our collection to ensure that every child can find something they'll love.</p>
     </div>
   </div>
+</div>
 </div>
     );
 };
--- src/pages/Home/Products/Product.jsx
+++ src/pages/Home/Products/Product.jsx
@@ -4,12 +4,12 @@ const Product = ({ product }) => {
     const { title, img } = product;
     return (
         <div className="card w-96 glass">
-            <figure><img  className='h-[300px] w-[800px] hover:opacity-100' src={img} alt="car!" /></figure>
+            <figure><img  className='h-[300px] w-[800px]' src={img} alt="car!" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{title}</h2>
+                <h2 data-aos="zoom-out-up" className="card-title">{title}</h2>
                 <p>How to park your car at your garage?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary hover:btn-accent">Learn now!</button>
+                <div className="card-actions justify-center">
+                    <button className="btn btn-primary hover:btn-accent">View Details!</button>
                 </div>
             </div>
         </div>
